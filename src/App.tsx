ffc2b70fc30aedb31580f4bb4c0536d3dd20d342
marco-soria <marco.soriaa7@gmail.com>
2024-06-16 17:51:28 -0500

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react"
+import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
+import { menuItems } from "./data/db"
+import { initialState, orderReducer } from "./reducers/order-reducer"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(orderReducer, initialState)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <header className=" bg-teal-400 py-5">
+          <h1 className="text-center text-4xl font-black">Tip Calculator and Total</h1>
+        </header>
+
+        <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+          <div className='p-5'>
+            <h2 className='font-black text-4xl'>Menu</h2>
+
+            <div className='mt-10 space-y-3'>
+              {menuItems.map(item => (
+                <MenuItem 
+                  key={item.id}
+                  item={item}
+                  dispatch={dispatch}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+            {state.order.length ? (
+              <>
+                  <OrderContents
+                    order={state.order}
+                    dispatch={dispatch}
+                  />
+                  <TipPercentageForm 
+                    dispatch={dispatch}
+                    tip={state.tip}
+                  />
+                  <OrderTotals 
+                    order={state.order}
+                    tip={state.tip}
+                    dispatch={dispatch}
+                  />
+              </>
+            ) : (
+              <p className="text-center">The Order is Empty</p>
+            )}
+
+          </div>
+        </main>
+
     </>
   )
 }
 
 export default App
+
---
+++ src/data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from '../types';
+
+export const menuItems: MenuItem[] = [
+    {
+      "id": 1,
+      "name": "Wood-fired Pizza Small",
+      "price": 30
+    },
+    {
+      "id": 2,
+      "name": "Wood-fired Pizza Medium",
+      "price": 50
+    },
+    {
+      "id": 3,
+      "name": "Piece of Lemon Pie",
+      "price": 30
+    },
+    {
+      "id": 4,
+      "name": "Piece of Chocolate Cake",
+      "price": 30
+    },
+    {
+      "id": 5,
+      "name": "Orange Juice",
+      "price": 15
+    },
+    {
+      "id": 6,
+      "name": "Wood-fired Pizza Large",
+      "price": 70
+    },
+    {
+      "id": 7,
+      "name": "Rib Eye 800g",
+      "price": 100
+    },
+    {
+      "id": 8,
+      "name": "Strawberry Juice",
+      "price": 15
+    },
+    {
+      "id": 9,
+      "name": "Tekila",
+      "price": 40
+    },
+    {
+      "id": 10,
+      "name": "Piece of Cheesecake",
+      "price": 30
+    },
+    {
+      "id": 11,
+      "name": "Americano Coffee",
+      "price": 20
+    },
+    {
+      "id": 12,
+      "name": "Capuccino Coffee",
+      "price": 40
+    }
+]
